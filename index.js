0aa3adec1d0e7ce495d8fcd3210538eccbeec51e
Felix Yeung <FelixYeung@Felixs-MacBook-Pro.local>
2020-11-26 20:30:38 +0800

    Cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 8010;
 
 const { LookupAddressLatLon } = require("./src/lookupAddressLatLon");
+
+app.use(cors());
 
 app.get("/v1/lookupAddressLatLon", async (req, res) => {
     const address = decodeURIComponent(req.query.address);

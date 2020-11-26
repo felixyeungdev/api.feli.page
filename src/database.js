00f4a005623164168cf2ffcab2f5ff94d9e336ff
Felix Yeung <FelixYeung@Felixs-MacBook-Pro.local>
2020-11-26 21:28:13 +0800

    Small fix for chinese characters

--- src/database.js
+++ src/database.js
@@ -4,14 +4,14 @@ class FeliDatabase {
     static setKeyValue(file, key, value) {
         const filePath = `database/${file}`;
         const dataJson = JSON.parse(fs.readFileSync(filePath, "utf8"));
-        dataJson[key] = value;
+        dataJson[encodeURIComponent(key)] = value;
         fs.writeFileSync(filePath, JSON.stringify(dataJson));
     }
 
     static getKeyValue(file, key) {
         const filePath = `database/${file}`;
         const dataJson = JSON.parse(fs.readFileSync(filePath, "utf8"));
-        return dataJson[key];
+        return dataJson[encodeURIComponent(key)];
     }
 }
 
